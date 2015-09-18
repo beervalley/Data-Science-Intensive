d85b741ed836f144f7cf1b6fa4dc1eb9a0b1d313
beervalley <gmfwcsdn@gmail.com>
2015-09-18 18:52:34 -0400

    feature extraction and selection

---
+++ Feature_Selection_Mini_Project/find_signature.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import pickle
+import numpy
+numpy.random.seed(42)
+
+
+### the words (features) and authors (labels), already largely processed
+### these files should have been created from the previous (Lesson 10) mini-project.
+words_file = "../text_learning/your_word_data.pkl" 
+authors_file = "../text_learning/your_email_authors.pkl"
+word_data = pickle.load( open(words_file, "r"))
+authors = pickle.load( open(authors_file, "r") )
+
+print len(word_data)
+print len(authors)
+
+
+### test_size is the percentage of events assigned to the test set (remainder go into training)
+### feature matrices changed to dense representations for compatibility with classifier
+### functions in versions 0.15.2 and earlier
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                             stop_words='english')
+features_train = vectorizer.fit_transform(features_train)
+features_test  = vectorizer.transform(features_test).toarray()
+
+feature_names = vectorizer.get_feature_names()
+
+### a classic way to overfit is to use a small number
+### of data points and a large number of features
+### train on only 150 events to put ourselves in this regime
+features_train = features_train[:150].toarray()
+labels_train   = labels_train[:150]
+
+
+### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+most_imp = clf.feature_importances_[0]
+number = 0
+index = -1
+
+for fea_imp in clf.feature_importances_:
+	index += 1
+	
+	if fea_imp > 0.2:
+		print "Index: %d, feature_importance: %f" % (index, fea_imp)
+	
+	if fea_imp > most_imp:
+		most_imp = fea_imp
+		number = index
+
+print "the importance of the most important feature:", most_imp
+print "the number of the most important feature:", number
+print 'Accuracy of the Decision Tree Classifier on the test set:', accuracy_score(labels_test, pred)
+
+print 'The name of the most important feature:', feature_names[14343]
+
+
